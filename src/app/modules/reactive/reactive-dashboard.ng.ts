96992def1a591794d82f9e21cdd9470fa51dd5b1
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2025-03-02 17:24:41 +0100

    refactor: remove dashboard containers

--- src/app/modules/reactive/reactive-dashboard.ng.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { ReactiveSimpleInputComponent } from './reactive-simple-input.ng';
-
-@Component({
-  selector: 'app-reactive-dashboard',
-  template: ` <app-reactive-simple-input /> `,
-  imports: [ReactiveSimpleInputComponent],
-})
-export class ReactiveDashboardComponent {}
--- src/app/modules/template/template-dashboard.ng.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { TemplateSimpleInputComponent } from './template-simle-input.ng';
-
-@Component({
-  selector: 'app-template-dashboard',
-  template: ` <app-template-simple-input /> `,
-  imports: [TemplateSimpleInputComponent],
-})
-export class TemplateDashboardComponent {}
